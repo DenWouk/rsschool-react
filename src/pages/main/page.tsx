d3f917407b1f42f18a3213b3e84af48a60394889
Den <info.d1@protonmail.com>
2023-11-24 02:25:23 +0300

    chore: transfer components from task module04

---
+++ src/pages/main/page.tsx
@@ -0,0 +1,12 @@
+import { Pagination } from "../../components/Pagination/Pagination";
+import { CardsList } from "../../components/CardsList/CardsList";
+import { PageSizeBtns } from "../../components/PageSizeBtns/PageSizeBtns";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import "./MainPage.css";
+
+export function MainPage(): JSX.Element {
+  return (
+    <h1>Hello!</h1>
+    
+  );
+}
