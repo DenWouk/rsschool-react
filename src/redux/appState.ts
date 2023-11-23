94dcf4e615aee746ed7cdfbf816196a1f207ff4c
Den <info.d1@protonmail.com>
2023-11-24 00:57:45 +0300

    init: create app template for task next-ssr-ssg

--- src/redux/appState.ts
+++
@@ -1,7 +0,0 @@
-import { AppStateInterface } from '../types/types';
-
-export const initialState: AppStateInterface = {
-  searchValue: localStorage.getItem('searchValue') || 'news',
-  page: Number(localStorage.getItem('page')) || 1,
-  pageSize: 10,
-};
